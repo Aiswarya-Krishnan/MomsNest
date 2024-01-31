d71b366ee7c0b3a84992ac4f7a67a8c913d7999e
melvin6456@gmail.com <melvin6756@gmail.com>
2024-01-31 14:03:01 +0530

    Add project files.

---
+++ MomsNest/Controllers/CategoryController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using MomsNest.Data;
+using MomsNest.Models;
+
+namespace MomsNest.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly AppDbContext context;
+
+        public CategoryController(AppDbContext context)
+        {
+            this.context = context;
+        }
+        public IActionResult Index()
+        {
+            List<Category> CategoryList = context.Categories.ToList();
+            return View(CategoryList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            if (category.Name == category.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name","Name and Display Order Should not be same");
+            }
+           
+
+            if (ModelState.IsValid)
+            {
+                context.Categories.Add(category);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+                return View();
+        }
+    }
+}
---
+++ MomsNest/Data/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using MomsNest.Models;
+
+namespace MomsNest.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+                
+        }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { CategoryId=1,Name="Footwear",DisplayOrder=1},
+                new Category { CategoryId = 2, Name = "Toys", DisplayOrder = 2 },
+                new Category { CategoryId =3, Name = "Hygiene", DisplayOrder = 3 }
+
+                );
+        }
+
+    }
+}
